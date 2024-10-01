1eccd83a0bc149e6fd787ab00d8c7bb9db211132
Andres-VanegasH <andres.vanegas.0193@gmail.com>
2024-10-01 18:35:40 -0500

    Primer ejercicio

---
+++ ejemplo.py
@@ -0,0 +1,13 @@
+#https://www.python.org/
+
+"""
+https://blog.hubspot.es/
+website/hacer-comentarios-python
+"""
+
+variable = "Esto es una variable de tipo string"
+numero= 20 #variable de tipo numerica (int o float)
+verdadero = False #variable de tipo booleano
+GRAVEDAD = 9.8 #Constante 
+
+print("Hola, esto es Python")
